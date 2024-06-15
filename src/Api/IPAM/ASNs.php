f64b74080c558c41bb0b95df306eb7aa3becc758
Gijsbert <gijsbert@dutchis.net>
2024-06-15 12:35:31 +0200

    Add ASNs

---
+++ src/Api/IPAM/ASNs.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace gjsbrt\NetBox\Api\IPAM;
+
+use gjsbrt\NetBox\Api\AbstractApi;
+
+class ASNs extends AbstractApi
+{
+    /**
+     * @param $params
+     * @return mixed
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function add($params=[])
+    {
+        return $this->post("/ipam/asns/", $params);
+    }
+
+    /**
+     * @param $params
+     * @return mixed
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function remove($id, $params=[])
+    {
+        return $this->delete("/ipam/asns/".$id."/", $params);
+    }
+
+    /**
+     * @param $params
+     * @return mixed
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function edit($id, $params=[])
+    {
+        return $this->put("/ipam/asns/".$id."/", $params);
+    }
+
+    /**
+     * @param $params
+     * @return mixed
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function list($params=[])
+    {
+        return $this->get("/ipam/asns/", $params);
+    }
+
+    /**
+     * @param $params
+     * @return mixed
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function show($id, $params=[])
+    {
+        return $this->get("/ipam/asns/".$id."/", $params);
+    }
+}
